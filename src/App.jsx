abee99dcb83e31daf55af5ffa2fb7de4e0ad7e70
alice <abdeeva_alisa21@mail.ru>
2023-12-04 18:46:33 +0300

    changed App

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-import React,  { useEffect } from 'react';
+import React from 'react';
 import Nav from './nav';
 import Footer from './footer';
 import Home from './home';
 import Shop from './shop';
-import { BrowserRouter as Router, Route, Routes, useNavigate  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate  } from 'react-router-dom';
 import './styles/App.css';
 import Cart from './cart';
 import About from './about';
 import Login from './Login';
 import Registration from './Registration';
 const App = () => {
-  const navigate = useNavigate();
-  // Перенаправление на домашнюю страницу при загрузке
-  useEffect(() => {
-    navigate('/');
-  }, [navigate]);
-
   return (
     <Router>
       <>
         <Nav />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/cart" element={<Cart />} /> 
           <Route path="/about" element={<About/>}></Route>
