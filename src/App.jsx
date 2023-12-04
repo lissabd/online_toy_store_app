2acb57f8977ff831cea5f073c10771b4becaa57e
alice <abdeeva_alisa21@mail.ru>
2023-12-04 18:39:19 +0300

    add navigation in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import React, {useState} from 'react';
+import React,  { useEffect } from 'react';
 import Nav from './nav';
 import Footer from './footer';
 import Home from './home';
 import Shop from './shop';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useNavigate  } from 'react-router-dom';
 import './styles/App.css';
 import Cart from './cart';
 import About from './about';
 import Login from './Login';
 import Registration from './Registration';
 const App = () => {
+  const navigate = useNavigate();
+  // Перенаправление на домашнюю страницу при загрузке
+  useEffect(() => {
+    navigate('/');
+  }, [navigate]);
+
   return (
     <Router>
       <>
         <Nav />
-        <Home/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
